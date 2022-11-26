4cd50b65932be19426d026ac3b911ce70a0d1078
hm0421 <117977500+hm0421@users.noreply.github.com>
2022-11-25 21:03:58 -0800

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,6 @@
+# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
